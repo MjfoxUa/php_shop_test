6c07e5b5cdbb00d58dbde97ee47d4a3055d13c09
Viktor Osadchuk <viktor.osadchuk@plumrocket.com>
2021-02-27 07:09:58 +0200

    Minor change

--- App/Core/Api/UrlBuilderInterface.php
+++ App/Core/Api/UrlBuilderInterface.php
@@ -1,18 +1,11 @@
 <?php
 /**
- * Plumrocket Inc.
- *
+ * MjFox Inc.
  * NOTICE OF LICENSE
  *
- * This source file is subject to the End-user License Agreement
- * that is available through the world-wide-web at this URL:
- * http://wiki.plumrocket.net/wiki/EULA
- * If you are unable to obtain it through the world-wide-web, please
- * send an email to support@plumrocket.com so we can send you a copy immediately.
- *
- * @package     Plumrocket_php_shop_test
- * @copyright   Copyright (c) 2021 Plumrocket Inc. (http://www.plumrocket.com)
- * @license     http://wiki.plumrocket.net/wiki/EULA  End-user License Agreement
+ * @package     MjFox_SHOP
+ * @copyright   Copyright (c) 2021 MjFox Inc.
+ * @license     End-user License Agreement
  */
 
 namespace App\Core\Api;
--- App/Core/ObjectManager.php
+++ App/Core/ObjectManager.php
@@ -3,11 +3,10 @@
  * MjFox Inc.
  * NOTICE OF LICENSE
  *
- * @package     MjFox shop
- * @copyright   Copyright (c) 2019 MjFox Inc. (http://www.mjfox.com)
- * @license     http://wiki.mjfox.com/wiki/EULA  End-user License Agreement
+ * @package     MjFox_SHOP
+ * @copyright   Copyright (c) 2021 MjFox Inc.
+ * @license     End-user License Agreement
  */
-
 namespace App\Core;
 
 class ObjectManager
